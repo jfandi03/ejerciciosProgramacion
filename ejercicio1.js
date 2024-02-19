7f5b1e6c333b7ad2cb6efd93795f754bcac053c8
Jonathan Fandino <jfandi03@gmail.com>
2024-02-19 11:26:04 -0500

    taller de ejercicios resueltos

--- ejercicio1.js
+++ ejercicio1.js
@@ -194,4 +194,183 @@ console.log(persona.edad)
 
 
 
+// Explicaciones Lunes 19
 
+function calculaMatricula (estrato) {
+  let totalAPagar = 100000
+  if (estrato <= 3) return totalAPagar = 0;
+  if (estrato <= 5) return totalAPagar;
+  if (estrato <= 8) return totalAPagar*1.2;
+}
+calculaMatricula (8)
+console.log("El estudiante paga " + calculaMatricula (8))
+
+
+// Ejercicio 1
+
+let nombreCliente = "Jonathan"
+let producto = "Computador"
+let precioProducto = 1500000
+let cantidad = 2
+let totalAPagar = cantidad*precioProducto
+    
+console.log ("El cliente " + nombreCliente + " adquirió la cantidad de " 
++ cantidad + " unidades del producto " + producto + " , para un total a pagar de $" + totalAPagar + " de pesos")
+
+
+
+// Ejercicio 2
+
+let frutas = ["Manzana",  "Pera", "Fresa", "Granadilla", "Papaya", "Mandarina"]
+
+console.log (frutas [0] + " y " +frutas[5])
+
+
+
+// Ejercicio 2 de otra forma
+
+let frutas = ["Manzana",  "Pera", "Fresa", "Granadilla", "Papaya", "Mandarina"]
+
+console.log (frutas [0] + " y " + frutas[frutas.length-1])
+
+// Ejercicio 3
+function generarTabla(numero){
+  for (i = 1; i <= 10; i ++)
+  console.log(numero + " X " + i + " = " + numero*i)
+}
+generarTabla (3)
+
+
+// Ejercicio 4
+function sumar(num1, num2){
+  return console.log ( num1 + num2);
+}
+sumar (3,2)
+
+
+// Ejercicio 4a
+function sumar(num1, num2){
+  return num1 + num2;
+}
+console.log (sumar (3,2))
+
+
+
+
+// Ejercicio 5
+
+function calificar (puntuacion){
+  if (puntuacion >= 3){
+    return "Aprobado";
+  }
+  else if (puntuacion < 3 && puntuacion >= 2) {
+    return "Reprobado";
+  }
+  else {
+    return "Reprobado, estudia más"
+  }
+  }
+console.log(calificar(1))
+
+
+// Ejercicio 5a
+
+function calificar (puntuacion){
+  if (puntuacion >= 3) return "Aprobado";
+  if (puntuacion < 3 && puntuacion >= 2) return "Reprobado";
+  
+    return "Reprobado, estudia más"
+  
+  }
+console.log(calificar(3))
+
+
+// Ejercicio 6
+
+let terrestres = ["perro", "gato"]
+let acuaticos = ["delfín", "tiburón"]
+let animales = [...terrestres, ...acuaticos]
+console.log(animales)
+
+// Ejercicio 6 a
+
+let terrestres = ["perro", "gato"]
+let acuaticos = ["delfín", "tiburón"]
+let animales = terrestres.concat(acuaticos);
+console.log(animales)
+
+
+
+// Ejercicio 7
+
+let mascota ={
+  nombre : "Tony",
+  tipo : "gato",
+}
+if (mascota.tipo == "Perro") {
+  console.log ("Me regalaron un perrito")
+} 
+else if (mascota.tipo == "gato"){
+  console.log("Me regalaron un gato")
+}
+
+// Ejercicio 7 con el operador ||
+
+let mascota ={
+  nombre : "Tony",
+  tipo : "gato",
+}
+if (mascota.tipo == "Perro" || mascota.tipo == "perro") {
+  console.log ("Me regalaron un perrito")
+} 
+else if (mascota.tipo == "Gato" || mascota.tipo == "gato"){
+  console.log("Me regalaron un gato")
+}
+else{
+  console.log("Mi mascota no es ni perro ni gato")
+}
+
+
+// Ejercicio 8
+
+let estudiante = {
+  nombre : "L",
+  edad : 18,
+  curso : "Programación",
+  telefono : 3046678587
+}
+
+console.log (estudiante.telefono)
+
+
+// Ejercicio 9
+
+// Ejercicio 9
+function generarFactura (
+  nombreCliente, producto, precioProducto, cantidad){
+    let totalAPagar = cantidad*precioProducto;
+    console.log ("El cliente " + nombreCliente + " adquirió la cantidad de " 
+  + cantidad + " unidades del producto " + producto + " , para un total a pagar de $" + totalAPagar + " de pesos")}
+    generarFactura ("Jonathan", "computador", 1500000, 2)
+
+
+// Ejercicio 9 lo mismo pero con return
+function generarFactura (
+  nombreCliente, producto, precioProducto, cantidad){
+    let totalAPagar = cantidad*precioProducto;
+    return ("El cliente " + nombreCliente + " adquirió la cantidad de " 
+  + cantidad + " unidades del producto " + producto + " , para un total a pagar de $" + totalAPagar + " de pesos")}
+    generarFactura ("Jonathan", "computador", 1500000, 2)
+
+
+
+    // Ejercicio 10
+function esFinDeSemana (dia){
+  if (dia == 6 || dia == 7){
+    console.log(true);
+  }
+  else {
+    console.log(false)
+  } 
+}
+esFinDeSemana (6)
